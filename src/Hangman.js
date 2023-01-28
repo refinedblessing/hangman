1f17df074ba64081d4d8c83dd736532e605db3f4
Blessing Edugie Ebowe <blessingebowe@gmail.com>
2023-01-28 11:17:34 -0800

    Use random word

--- src/Hangman.js
+++ src/Hangman.js
@@ -7,12 +7,14 @@ import img3 from './3.jpg';
 import img4 from './4.jpg';
 import img5 from './5.jpg';
 import img6 from './6.jpg';
+import { randomWord } from './words';
 
 class Hangman extends Component {
   /** by default, allow 6 guesses and use provided gallows images. */
   static defaultProps = {
     maxWrong: 6,
     images: [img0, img1, img2, img3, img4, img5, img6],
+    answer: randomWord(),
   };
 
   constructor(props) {
@@ -22,7 +24,6 @@ class Hangman extends Component {
       gameOver: false,
       nWrong: 0,
       guessed: new Set(),
-      answer: 'apple',
     };
     this.handleGuess = this.handleGuess.bind(this);
   }
@@ -31,10 +32,10 @@ class Hangman extends Component {
     if guessed letters are {a,p,e}, show "app_e" for "apple"
   */
   guessedWord() {
-    const { answer, guessed, gameOver } = this.state;
-    return answer.split('').map((ltr) => (
-      (guessed.has(ltr) || gameOver) ? ltr : '_'
-    ));
+    const { guessed, gameOver } = this.state;
+    return this.props.answer
+      .split('')
+      .map((ltr) => (guessed.has(ltr) || gameOver ? ltr : '_'));
   }
 
   /** handleGuess: handle a guessed letter:
@@ -44,11 +45,12 @@ class Hangman extends Component {
   handleGuess(e) {
     const char = e.target.value;
     this.setState((oldState) => {
-      const { guessed, nWrong, answer } = oldState;
-      const newGuessed = guessed.add(char)
+      const { guessed, nWrong } = oldState;
+      const { answer } = this.props;
+      const newGuessed = guessed.add(char);
       // check if all answers have been provided
       const gameWon = answer.split('').every((char) => guessed.has(char));
-  
+
       const newNWrong = nWrong + (answer.includes(char) ? 0 : 1);
       const gameOver = newNWrong === this.props.maxWrong;
       return {
@@ -90,9 +92,25 @@ class Hangman extends Component {
           You have {nWrong} wrong {nWrong !== 1 ? 'guesses' : 'guess'}
         </p>
         <p className="Hangman-word">{this.guessedWord()}</p>
-        {(gameOver && !gameWon) && <p>You Lost!!! <span aria-label='loser' role='img'>🤪</span></p>}
-        {gameWon && <p>You Won <span aria-label='gameWon' role='img'>🏆</span></p>}
-        {(!gameOver && !gameWon) && <p className="Hangman-btns">{this.generateButtons()}</p>}
+        {gameOver && !gameWon && (
+          <p>
+            You Lost!!!{' '}
+            <span aria-label="loser" role="img">
+              🤪
+            </span>
+          </p>
+        )}
+        {gameWon && (
+          <p>
+            You Won{' '}
+            <span aria-label="gameWon" role="img">
+              🏆
+            </span>
+          </p>
+        )}
+        {!gameOver && !gameWon && (
+          <p className="Hangman-btns">{this.generateButtons()}</p>
+        )}
       </div>
     );
   }
